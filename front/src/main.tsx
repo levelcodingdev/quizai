7187e64ab227ca997d1229cb150bbcae45022b6e
levelcodingdev <dev@levelcoding.com>
2025-09-04 21:05:48 +0300

    Update main.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
 import { Toaster } from 'react-hot-toast'
 
